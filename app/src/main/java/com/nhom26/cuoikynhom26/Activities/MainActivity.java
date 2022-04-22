9576876f3b3b28549276d88d6a72d09efab4d29e
long230796 <long230796@gmail.com>
2022-04-22 11:11:16 +0700

    Thêm activity_nguyen_lieu.xml

--- app/src/main/java/com/nhom26/cuoikynhom26/Activities/MainActivity.java
+++ app/src/main/java/com/nhom26/cuoikynhom26/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nhom26.cuoikynhom26;
+package com.nhom26.cuoikynhom26.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nhom26.cuoikynhom26.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/nhom26/cuoikynhom26/Activities/NguyenLieuActivity.java
@@ -0,0 +1,15 @@
+package com.nhom26.cuoikynhom26.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nhom26.cuoikynhom26.R;
+
+public class NguyenLieuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nguyen_lieu);
+    }
+}
