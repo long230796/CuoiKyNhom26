525d4b07a251e35ba32c53e98c509e8f04912f79
long230796 <long230796@gmail.com>
2022-04-22 09:03:05 +0700

    Khởi tạo project

---
+++ app/src/main/java/com/nhom26/cuoikynhom26/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nhom26.cuoikynhom26;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
