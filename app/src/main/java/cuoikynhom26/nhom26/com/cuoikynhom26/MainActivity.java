9da424b22d9f8c60d5c6c90517a35384e3f8d29a
Quang Le <nhutquang097@gmail.com>
2022-04-29 21:33:49 +0700

    pull

---
+++ app/src/main/java/cuoikynhom26/nhom26/com/cuoikynhom26/MainActivity.java
@@ -0,0 +1,13 @@
+package cuoikynhom26.nhom26.com.cuoikynhom26;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
